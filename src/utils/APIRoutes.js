138d8a5c821c9c668dec49597a2aba6fc03e9f22
Mahmoud_Hamza <86957735+Mahmoud-Hamza-Git@users.noreply.github.com>
2023-10-04 22:31:52 +0300

    Update APIRoutes.js

--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "https://snapy-app.onrender.com";
+export const host = "https://snapy-server-side.onrender.com";
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
